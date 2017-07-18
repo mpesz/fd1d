5550bcb3e9f21ecdcf30864b7b772224e46fb08a
mpesz <mpesz@users.noreply.github.com>
2017-07-17 18:22:53 -0700

    Add files via upload

--- fd1d_heat.m
+++ fd1d_heat.m
@@ -1,10 +1,10 @@
-function [xplot,nsols] = fd1d_heat (nx,dt,bdaryflag,outflag) 
+function [xplot] = fd1d_heat (nx,dt,bdaryflag,outflag) 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% fd1d_heat (nx,dt,bdaryflag,outflag)
-%% 1D FD cell-centered solution to heat equation on (0,1)
+%% 1D FD cell-centered solution to heat equation on (xbeg,xend)
 %% user must code the functions permfun, porfun, rhsfunt, 
 %%    initfun, exfun, dexfun INSIDE the code (below)
-%% nx is the number of sub-intervals of (0,1)
+%% nx is the number of sub-intervals of (0,1) for uniform grid
 %% dt is the time step
 %% dt == 0: steady state solution only
 %% bdaryflag == 0: Dirichlet (nonhomogeneous) BC on both ends
@@ -18,65 +18,66 @@ function [xplot,nsols] = fd1d_heat (nx,dt,bdaryflag,outflag)
 %% nsols : the numerical solution
 %%   the code will also plot the numerical solution
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+xbeg = 0; xend = 1;               %% or change to another interval
+dx = (xend-xbeg)/nx * ones(nx,1); %% or code the nonuniform grid 
 
-dx = 1/nx * ones(nx,1); x0 = 0; 
 
-t1 = 0; t2 = 1;
+t1 = 0; t2 = 1;     %% or use a different time interval
 if dt == 0 
-    nt = 1; transient = false;  
+    nt = 1; transient = false;  dt = 1;
 else 
      transient = true; nt = (t2-t1) / dt; 
 end;
 
 %%%%% create data structures
-x = dx; x(1)=x0; for i=2:nx x(i)=x(i-1)+dx(i);end;
-xplot = x + dx/2;
+x = linspace(xbeg,xend-dx(nx),nx)'; %% x is the left end of each subinterval
+xplot = x + dx/2;  %% xplot is the center of each subinterval
 
 %% permeability coefficient
-perm = x; perm(:,1) = permfun(x(:,1)+ dx(:,1)/2);
+perm = 0*xplot; perm = permfun(xplot); %#ok<*NASGU>
 
 %% porosity coefficient
-if transient, por = x; por(:,1) = porfun(x(:,1)+ dx(:,1)/2);end;
+if transient, por = 0*x; por= porfun(xplot);end
 
 %%%%% compute transmissibilities
 tx = zeros(nx+1,1); 
-for i=2:nx 
-       tx(i)=2/(dx(i-1)/perm(i-1)+dx(i)/perm(i));
-end;
+for j=2:nx 
+       tx(j)=2/(dx(j-1)/perm(j-1)+dx(j)/perm(j));
+end
  
 if bdaryflag == 0     %% Dirichlet contributions to the transmissibilities
-  i = 1;         tx(i)=1/(dx(i)/perm(i));
-  i = nx + 1;    tx(i)=1/(dx(i-1)/perm(i-1));
+  j = 1;         tx(j)=1/(dx(j)/perm(j));
+  j = nx + 1;    tx(j)=1/(dx(j-1)/perm(j-1));
 end
-
 tx = tx*dt;
     
 stiff = sparse (nx,nx);
-
-for i=2:nx                                       
-  gl = i-1;                                             
-  gr = i;
-  stiff(gl,gl) = stiff(gl,gl) + tx(i);                                    
-  stiff(gl,gr) = stiff(gl,gr) - tx(i);                                                              
-  stiff(gr,gl) = stiff(gr,gl) - tx(i);                                    
-  stiff(gr,gr) = stiff(gr,gr) + tx(i);     
+for j=2:nx                                       
+  gl = j-1;                                             
+  gr = j;
+  stiff(gl,gl) = stiff(gl,gl) + tx(j);                                    
+  stiff(gl,gr) = stiff(gl,gr) - tx(j);                                                              
+  stiff(gr,gl) = stiff(gr,gl) - tx(j);                                    
+  stiff(gr,gr) = stiff(gr,gr) + tx(j);     
 end;
     
 %% Dirichlet contributions to matrix
 if bdaryflag == 0
-    i = 1; gr = 1;       stiff(gr,gr) = stiff(gr,gr) + tx(i);     
-    i = nx + 1; gl = nx; stiff(gl,gl) = stiff(gl,gl) + tx(i);  
+    j = 1; gr = 1;       stiff(gr,gr) = stiff(gr,gr) + tx(j);     
+    j = nx + 1; gl = nx; stiff(gl,gl) = stiff(gl,gl) + tx(j);  
 end
-errvec = [];
+
+%
+errvec = zeros(nt,1);
 
 if transient
-    for i=1:nx
-        stiff(i,i) = stiff(i,i) + por(i)*dx(i);
-    end;
-    nsol = initfun ( x + dx/2 );    
-end;
+    for j=1:nx
+        stiff(j,j) = stiff(j,j) + por(j)*dx(j); %#ok<SPRIX>
+    end
+    nsol = initfun ( xplot);    
+end
 
-nsols = [];
+%nsols = [];
 for n = 1 : nt %  time step loop
     t = t1 + n*dt;
     
@@ -87,11 +88,11 @@ for n = 1 : nt %  time step loop
     end;
     
     if bdaryflag == 0
-        val1 = exfun(0,t-dt/2);
-        val2 = exfun(1,t-dt/2);
+        val1 = exfun(xbeg,t-dt/2);
+        val2 = exfun(xend,t-dt/2);
     else
-        val1 = - dexfun(0,t-dt/2);
-        val2 =  dexfun(1,t-dt/2);
+        val1 = - dexfun(xbeg,t-dt/2);
+        val2 =  dexfun(xend,t-dt/2);
     end;
     
     %% contributions of bdary conditions to the rhs 
@@ -106,7 +107,7 @@ for n = 1 : nt %  time step loop
 %%%%% find numerical solution
     nsol = stiff \ q;
  
-    nsols = [nsols, nsol];
+    %% nsols = [nsols, nsol];
     if outflag == 0
         plot(xplot,nsol,'r*-'),
         axis([0 1 -1 1]);
@@ -117,44 +118,51 @@ for n = 1 : nt %  time step loop
         plot(xplot,sol,'b',xplot,nsol,'r*-'),
         axis ([0 1 0 0.5]);
         pause(.1);
-        errvec = [errvec; norm(sol-nsol,inf)];
+        errvec (n)= norm(sol-nsol,inf);
     end
 end
 if outflag ~= 0 
     errornorm = max(errvec);
     fprintf('dx = %g dt = %g error = %g \n',max(dx),dt,errornorm);
-end;
+end
+
+end  %% function
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Comment/Uncomment related examples below, or code/create your own. 
 %% Ensure input parameters are consistent with choices below.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function v = permfun(x)
-%v = 1;
-v = zeros(size(x,1),1);
-for j=1:size(x,1)
-    if abs(x(j)-.3) < 0.2 v(j) = 1e-1;
-    else v(j) = 1e0;
-    end;
-end;
+%%
+v = zeros(size(x,1),1) + 1;
+%% uncomment to have a low permeability zone 
+% v ( find (abs(x-0.3)<0.2)) = 1e-1;
+end
 
 function v = porfun(x)
-v = 1;
+%% porosity 
+v = 1 + 0*x;
+%% realistic porosities are < 1.0
+end
 
 function v = rhsfunt(x,t)
 v = 2*(t) + x.*(1-x);
 %v = zeros(size(x));
+end
 
 function v = initfun(x)
 v = exfun(x,0);
 %v = zeros(size(x));
+end
 
 function v = exfun(x,t)
 v = (t)*x.*(1-x); %% with dirichlet: 0,0 first order, with flux -1,-1 quadratic (superconvergent)
 %v = zeros(size(x));
 %v = x;
+end
 
 function v = dexfun (x,t)
 v = (t)*(1-2*x);
 %v = zeros(size(x));
 %v = 1;
+end
